9683e56fe461845693d63627db41d3a799e3727f
chuchuh104 <bes359542@gmail.com>
2024-12-02 11:31:07 +0500

    Добавьте файлы проекта.

---
+++ YieldReturn/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace YieldDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(StartTextBox.Text, out int start) && int.TryParse(EndTextBox.Text, out int end))
+            {
+                var generator = new NumberGenerator();
+                NumberListBox.Items.Clear();
+
+                int totalNumbers = end - start + 1;
+                ProgressIndicator.Maximum = totalNumbers;
+                ProgressIndicator.Value = 0;
+
+                foreach (var number in generator.GenerateNumbers(start, end))
+                {
+                    NumberListBox.Items.Add(number);
+                    ProgressIndicator.Value++;
+
+                    // Ждем для имитации асинхронного процесса
+                    await Task.Delay(100);
+                }
+
+                MessageBox.Show("Генерация завершена!");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числа.");
+            }
+        }
+    }
+}
+
---
+++ YieldReturn/NumberGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class NumberGenerator
+{
+    public IEnumerable<int> GenerateNumbers(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            yield return i;
+        }
+    }
+}
+
